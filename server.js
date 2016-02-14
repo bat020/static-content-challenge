a9c48712002832c338c454e26618791165014a44
Anindya Bhattacharyya <bat020@gmail.com>
2016-02-14 16:42:58 +0000

    refactored server.js slightly

--- server.js
+++ server.js
@@ -5,19 +5,26 @@ const http = require('http');
 const url = require('url');
 const markdown = require('markdown').markdown;
 
+// declare constants
+
+const portNumber = process.env.PORT || 8081;
+const errorPage = '<!doctype html>\n<html>\n<head><title>' +
+  'Welcome to Acme</title></head>\n<body>\n<h1>404 error' +
+  '</h1>\n<p>That page does not exist.<p>\n</body>\n</html>';
+
 // main loop
 
 http.createServer((request, response) => {
-  fs.readFile('template.html', (error, template) => {
-    template = error ? '' : template.toString();
+  fs.readFile('template.html', (error, data) => {
+    const template = getFileData(error, data);
     const route = getRoute(request);
     renderTemplate(template, route, response);
   });
-}).listen(process.env.PORT || 8081);
+}).listen(portNumber);
 
 function renderTemplate(template, route, response) {
-  fs.readFile(getPath(route), (error, content) => {
-    content = error ? '' : content.toString();
+  fs.readFile(getPath(route), (error, data) => {
+    const content = getFileData(error, data);
     const page = buildPage(template, content);
     servePage(page, route, response);
   });
@@ -26,11 +33,11 @@ function renderTemplate(template, route, response) {
 function servePage(page, route, response) {
   const status = page ? 200 : 404;
   response.writeHead(status, {'Content-Type': 'text/html'});
-  response.end(page || errorPage());
+  response.end(page || errorPage);
   console.log(`GET ${route} => status code ${status}`);
 }
 
-// page building functions
+// page builder
 
 function buildPage(template, content) {
   if (!template || !content) return '';
@@ -38,13 +45,11 @@ function buildPage(template, content) {
   return template.replace('{{content}}', html);
 }
 
-function errorPage() {
-  return '<!doctype html>\n<html>\n<head><title>Welcome to ' +
-    'Acme</title></head>\n<body>\n<h1>404 error</h1>\n' +
-    '<p>That page does not exist.<p>\n</body></html>';
-}
-
 // miscellaneous helpers
+
+function getFileData(error, data) {
+  return error ? '' : data.toString();
+}
 
 function getRoute(request) {
   return url.parse(request.url).pathname;
@@ -56,4 +61,4 @@ function getPath(route) {
 
 // initial console message
 
-console.log('Server running at http://localhost:8081');
+console.log(`Server running at http://localhost:${portNumber}`);
